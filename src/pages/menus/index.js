c33520e9587457b2e175d119c44e8ec4e08d0b09
semicolon-id <ramabagas872@gmail.com>
2023-10-13 10:31:14 +0700

    update global.css

---
+++ src/pages/menus/index.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+ <div className="text-xl text-red-400 font-bold">menu</div>
+  );
+}
